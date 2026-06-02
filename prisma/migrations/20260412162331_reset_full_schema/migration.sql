6e26489944a6a744f2a0fd63679cf5cbcffc2dc0
mouha080 <walkerkyleissue@gmail.com>
2026-06-02 03:30:21 +0000

    fix: clean migration for production

--- prisma/migrations/20260412162331_reset_full_schema/migration.sql
+++ prisma/migrations/20260412162331_reset_full_schema/migration.sql
@@ -1,49 +1,34 @@
 -- DropForeignKey
-ALTER TABLE `sale` DROP FOREIGN KEY `Sale_productId_fkey`;
-
+ALTER TABLE `Sale` DROP FOREIGN KEY `Sale_productId_fkey`;
 -- DropForeignKey
-ALTER TABLE `stockmovement` DROP FOREIGN KEY `StockMovement_productId_fkey`;
-
+ALTER TABLE `StockMovement` DROP FOREIGN KEY `StockMovement_productId_fkey`;
 -- DropForeignKey
-ALTER TABLE `supplierpayment` DROP FOREIGN KEY `SupplierPayment_supplierId_fkey`;
-
+ALTER TABLE `SupplierPayment` DROP FOREIGN KEY `SupplierPayment_supplierId_fkey`;
 -- DropForeignKey
-ALTER TABLE `supply` DROP FOREIGN KEY `Supply_productId_fkey`;
-
+ALTER TABLE `Supply` DROP FOREIGN KEY `Supply_productId_fkey`;
 -- DropForeignKey
-ALTER TABLE `supply` DROP FOREIGN KEY `Supply_supplierId_fkey`;
-
+ALTER TABLE `Supply` DROP FOREIGN KEY `Supply_supplierId_fkey`;
 -- DropIndex
-DROP INDEX `Sale_productId_fkey` ON `sale`;
-
+DROP INDEX `Sale_productId_fkey` ON `Sale`;
 -- DropIndex
-DROP INDEX `StockMovement_productId_fkey` ON `stockmovement`;
-
+DROP INDEX `StockMovement_productId_fkey` ON `StockMovement`;
 -- DropIndex
-DROP INDEX `SupplierPayment_supplierId_fkey` ON `supplierpayment`;
-
+DROP INDEX `SupplierPayment_supplierId_fkey` ON `SupplierPayment`;
 -- DropIndex
-DROP INDEX `Supply_productId_fkey` ON `supply`;
-
+DROP INDEX `Supply_productId_fkey` ON `Supply`;
 -- DropIndex
-DROP INDEX `Supply_supplierId_fkey` ON `supply`;
-
+DROP INDEX `Supply_supplierId_fkey` ON `Supply`;
 -- AlterTable
-ALTER TABLE `supply` ADD COLUMN `note` VARCHAR(191) NULL,
+ALTER TABLE `Supply` ADD COLUMN `note` VARCHAR(191) NULL,
     MODIFY `paidAmount` DOUBLE NOT NULL DEFAULT 0,
     MODIFY `remaining` DOUBLE NOT NULL DEFAULT 0;
-
 -- AddForeignKey
 ALTER TABLE `StockMovement` ADD CONSTRAINT `StockMovement_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
 -- AddForeignKey
 ALTER TABLE `Sale` ADD CONSTRAINT `Sale_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
 -- AddForeignKey
 ALTER TABLE `Supply` ADD CONSTRAINT `Supply_supplierId_fkey` FOREIGN KEY (`supplierId`) REFERENCES `Supplier`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
 -- AddForeignKey
 ALTER TABLE `Supply` ADD CONSTRAINT `Supply_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
 -- AddForeignKey
 ALTER TABLE `SupplierPayment` ADD CONSTRAINT `SupplierPayment_supplierId_fkey` FOREIGN KEY (`supplierId`) REFERENCES `Supplier`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
